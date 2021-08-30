3a9b8123185beef40aecb372f15bc08f9f59af33
Kevin Kunkel <kunkelkevin@yahoo.com>
2021-08-29 20:10:19 -0500

    more task_log implementation

--- task-tracker/client/src/components/Task/index.js
+++ task-tracker/client/src/components/Task/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-// import Task from "../Task";
+import Task_Log from "../Task_Log";
 import { useStoreContext } from "../../utils/GlobalState";
+import Auth from "../utils/auth";
 
 function Task(item) {
   const [state] = useStoreContext();
@@ -8,19 +9,23 @@ function Task(item) {
   const { description, _id } = item;
 
   console.log(state);
-  //   const filteredTasks = () => {
-  //     if (!_id) {
-  //       return state.task;
-  //     }
-  //     return state.task.filter((task) => {
-  //       return task.customer._id === _id;
-  //     });
-  //   };
-  //   console.log(filteredTasks());
+  const filteredTask_Log = () => {
+    if (!_id) {
+      return state.task_log;
+    }
+    return state.task_log.filter((task_log) => {
+      return task_log.task._id === _id;
+    });
+  };
+  console.log(filteredTasks_Log());
 
   return (
     <li>
       {description}
+      {/* {Auth.loggedIn() && (
+        <Task_Log _id={filteredTask_Log}
+        <p>Input or update the time you worked on each task in minutes.</p>
+      ) } */}
       {/* <ul>
         {filteredTasks().map((task) => (
           <task key={task._id} _id={task._id} name={task.name}></task>
---
+++ task-tracker/client/src/components/Task_Log/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useStoreContext } from "../../utils/GlobalState";
+
+function Task_Log(item) {
+  const [state] = useStoreContext();
+
+  const { description, _id } = item;
+
+  console.log(state);
+  //   const filteredTasks = () => {
+  //     if (!_id) {
+  //       return state.task;
+  //     }
+  //     return state.task.filter((task) => {
+  //       return task.customer._id === _id;
+  //     });
+  //   };
+  //   console.log(filteredTasks());
+
+  return (
+    <li>
+      {description}
+      {/* <ul>
+        {filteredTasks().map((task) => (
+          <task key={task._id} _id={task._id} name={task.name}></task>
+        ))}
+      </ul> */}
+    </li>
+  );
+}
+
+export default Task_Log;
